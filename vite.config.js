6e7feebe2d9331e680b37b74389bde9564afe0c0
claranceliberi <liberintwari@gmail.com>
2022-04-11 15:27:19 +0200

    refactor: removing uneccessary dependency

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Pages from 'vite-plugin-pages'
 import WindiCSS from 'vite-plugin-windicss'
-import VueTypeImports from 'vite-plugin-vue-type-imports'
 
 const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), Pages(), WindiCSS(),VueTypeImports(),],
+  plugins: [vue(), Pages(), WindiCSS()],
   resolve: {
     alias: [
       {
